c83c218115b74d34d19aa78c7181205c41ff9e1b
Your Name <you@example.com>
2022-01-06 08:56:33 +0700

    Add 1st version

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Welcome to DevOps of Mi AI<br><img src="https://www.miai.vn/wp-content/uploads/2022/01/Logo_web-2-768x456.png">'
+    return 'Welcome to DevOps of Mi AI<br><img src="https://www.miai.vn/wp-content/uploads/2022/01/Logo_web-2-768x456.png" width="20%" border=1>'
 
     
 if __name__ == '__main__':
