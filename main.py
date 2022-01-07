cfcd3c168758a86c6a1c9f40be96ac3919d240eb
Your Name <you@example.com>
2022-01-07 09:50:49 +0700

    Add 1st version

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Welcome to DevOps oSHB<br><img src="https://www.miai.vn/wp-content/uploads/2022/01/Logo_web-2-768x456.png" width="20%" border=1>'
+    return 'Welcome to DevOps oSHB<br><img src="https://www.shb.com.vn/wp-content/uploads/2016/03/Logo-SHB-VN.png" width="20%" border=1>'
 
     
 if __name__ == '__main__':
