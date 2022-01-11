b43c67af40623b44aac1253e2f4d075e96ce80c5
Your Name <you@example.com>
2022-01-12 00:59:22 +0700

    Test 4 Miai

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Welcome to DevOps oSHB<br><img src="https://www.shb.com.vn/wp-content/uploads/2016/03/Logo-SHB-VN.png" width="20%" border=1>'
+    return 'My DevOps is here!'
 
     
 if __name__ == '__main__':
